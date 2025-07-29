9a5897ab9f8424af98c24ee8f310ecc488cd4329
Syed Mohsin <mohsn1094@proton.me>
2025-07-29 22:22:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-[DEFAULT]
-# Account name
-account =projectsg16
-# Azure Storage account access key
-key =10cthiqNfjVNv0UpJkGY3X/Q4ouBd9hWxCjfWpkq0/SrUTSCoXHOfgHOlCrzDhoqQZf3HqlAAagF+AStYblMWw==
-# Container name
-container =upload
+# config.py
+
+# AWS region where your S3 bucket is located
+AWS_REGION = 'us-east-1'
+
+# Name of your S3 bucket for uploads
+S3_BUCKET_NAME = 'mycapstoneuploads'
